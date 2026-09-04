f38e0623daa36cfdf3fd8a2271dcceee56a8f97b
v.razuvaev <rasuvaeff@gmail.com>
2026-09-04 19:54:04 +0300

    Analyze calls returned from specification closures

--- src/Psalm/Internal/BuilderType.php
+++ src/Psalm/Internal/BuilderType.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
 use Psalm\Codebase;
 use Psalm\Context;
 use Psalm\Type;
@@ -132,6 +133,10 @@ final class BuilderType
 
         $first = $closure->stmts[0];
 
-        return $first instanceof Expression ? $first->expr : null;
+        return match (true) {
+            $first instanceof Expression => $first->expr,
+            $first instanceof Return_ => $first->expr,
+            default => null,
+        };
     }
 }
